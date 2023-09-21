f4fda0ea5bf569d80afab5350ccfaceebaf74991
Balpreet0 <85600107+Balpreet0@users.noreply.github.com>
2023-09-21 22:29:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,39 @@
-
-
-
+const url = 'http://api.weatherapi.com/v1/current.json?key=60d2228f57704a2c84193052231909&q=London&aqi=no'
 
 const options = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '2a46320730msh29b9e4703ae22aep14c6b1jsn6057929fe1af',
-		'X-RapidAPI-Host': 'national-weather-service.p.rapidapi.com'
+		'X-RapidAPI-Key': '60d2228f57704a2c84193052231909',
+		'X-RapidAPI-Host': 'api.weatherapi.com'
 	}
 };
-fetch('https://national-weather-service.p.rapidapi.com/zones/%7Btype%7D/%7BzoneId%7D/forecast',options)
-.then(response =>response.json())
-.then(response => console.log(response))
-.catch(err =>console.error(err));
+async function fetchWeatherData() {
+	try {
+		const response = await fetch(url, options);{
+
+
+			wind_kph.innerHTML = response.wind_kph
+		wind_degree.innerHTML = response.wind_degree
+		wind_dir.innerHTML = response.wind_dir
+		pressure_in.innerHTML = response.pressure_in
+		humidity.innerHTML = response.humidity
+		cloud.innerHTML = response.cloud
+		feelslike_c.innerHTML = response.feelslike_c
+		feelslike_f.innerHTML = response.feelslike_f
+		temp_c.innerHTML =response.temp_c
+		gust_mph.innerHTML = response.gust_mph
+		gust_kph.innerHTML = response.gust_kph
 
 
 
-// const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': '2a46320730msh29b9e4703ae22aep14c6b1jsn6057929fe1af',
-// 		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
-// 	}
-// };
+		};
+		const result = await response.json();  // If the API returns JSON, use .json() instead of .text()
 
-// try {
-// 	const response = await fetch(url, options);
-// 	const result = await response.text();
-// 	console.log(result);
-// } catch (error) {
-// 	console.error(error);
-// }
+		
+		
+
+
+		console.log(result) ;
 
 
 
@@ -39,24 +41,12 @@ fetch('https://national-weather-service.p.rapidapi.com/zones/%7Btype%7D/%7BzoneI
 
 
 
+	} catch (error) {
+		console.error(error);
+	}
+}
 
+fetchWeatherData();  // Invoke the async function
 
-
-// const url = 'https://national-weather-service.p.rapidapi.com/zones/%7Btype%7D/%7BzoneId%7D/forecast';
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': '2a46320730msh29b9e4703ae22aep14c6b1jsn6057929fe1af',
-// 		'X-RapidAPI-Host': 'national-weather-service.p.rapidapi.com'
-// 	}
-// };
-
-// try {
-// 	const response = await fetch(url, options);
-// 	const result = await response.text();
-// 	console.log(result);
-// } catch (error) {
-// 	console.error(error);
-// }
 
 
